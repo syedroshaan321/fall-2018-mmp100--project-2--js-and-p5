a76be77bd5e08790d8c60c0575a0fe1118065f85
syedroshaan321 <43096419+syedroshaan321@users.noreply.github.com>
2018-11-03 12:48:24 -0400

    modify

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  background(128,128,128);
+}
+function draw(){
+ line(407,162,424,122);
+ point(385,139,234,222);
+ line(152,72,113,146);}
+function paws(){
+ line(213,282,245,279);
+point(261,271,281,291);
+}
+function ear(){
+ line(284,243,329,301);
+}
